5588cc999f9e304d26bf1f55ff62d464b41fbea8
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2020-07-27 09:56:21 +0530

    update banner-home

--- src/components/Home/HomeBanner1.vue
+++ src/components/Home/HomeBanner1.vue
@@ -1,36 +1,69 @@
 <template>
   <div class="home-banner">
-    <v-row class="pa-0 ma-0">
-      <v-col cols="12" class="pa-0 ma-0">
-        <v-img src="../../assets/homebanner-1.jpg" height="80vh" contain>
-          <v-overlay absolute opacity="0.6">
-            <v-row class="pb-0" app>
-              <v-col cols="12" sm="12" class="text-center">
-                <div class="text-h3 text-center font-weight-bold mb-4">
-                  Experimental & Experienced Learning
-                </div>
-                <div class="text-center">
-                  <v-btn
-                    class="ma-4 green"
-                    text
-                    color="white"
-                    to="/about"
-                  >
-                    Read More
-                  </v-btn>
-                  <v-btn
-                    class="ma-4 green"
-                    text
-                    color="white"
-                    to="apply"
-                  >
-                    Apply Now
-                  </v-btn>
-                </div>
-              </v-col>
-            </v-row>
-          </v-overlay>
-        </v-img>
+    <v-row class="pa-0 ma-0 mt-lg-16">
+      <v-col cols="12" class="pa-0 ma-0 mt-5">
+        <div v-if="$vuetify.breakpoint.lgAndUp">
+          <v-img src="../../assets/homebanner-1.png" height="80vh" class="banner-width" contain>
+            <v-overlay absolute opacity="0.6">
+              <v-row class="pb-0" app>
+                <v-col cols="12" sm="12" class="text-center">
+                  <div class="text-h3 text-center font-weight-bold mb-4">
+                    Experimental & Experienced Learning
+                  </div>
+                  <div class="text-center">
+                    <v-btn
+                      class="ma-4 green"
+                      text
+                      color="white"
+                      to="/about"
+                    >
+                      Read More
+                    </v-btn>
+                    <v-btn
+                      class="ma-4 green"
+                      text
+                      color="white"
+                      to="apply"
+                    >
+                      Apply Now
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-overlay>
+          </v-img>
+        </div>
+        <div v-else-if="$vuetify.breakpoint.mdAndDown">
+          <v-img src="../../assets/homebanner-1.png" height="80vh">
+            <v-overlay absolute opacity="0.6">
+              <v-row class="pb-0" app>
+                <v-col cols="12" sm="12" class="text-center">
+                  <div class="text-h3 text-center font-weight-bold mb-4">
+                    Experimental & Experienced Learning
+                  </div>
+                  <div class="text-center">
+                    <v-btn
+                      class="ma-4 green"
+                      text
+                      color="white"
+                      to="/about"
+                    >
+                      Read More
+                    </v-btn>
+                    <v-btn
+                      class="ma-4 green"
+                      text
+                      color="white"
+                      to="apply"
+                    >
+                      Apply Now
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-overlay>
+          </v-img>
+        </div>
       </v-col>
     </v-row>
   </div>
@@ -48,4 +81,7 @@ export default {
   min-width: 100vw !important;
 }
 
+.banner-width {
+  max-width: 100vw !important;
+}
 </style>
